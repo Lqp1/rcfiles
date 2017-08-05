98cbf9ad27152d7f97b511aa3c1851b232bc0aef
Thomas Lange <lqp1@users.noreply.github.com>
2017-08-05 11:44:07 +0200

    Now Uses oh-my-zsh and Ansible

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-FILES="screenrc vimrc zshrc bashrc dircolors zshenv"
-DIRS=".cache .cache/zsh .vimdat"
+# Check dependencies
+which ansible-playbook 2>&1 > /dev/null || exit 1
+which git 2>&1 > /dev/null || exit 1
 
-function copyfiles {
-TARGETUSER=${1}
-USERHOME=$(eval echo "~${TARGETUSER}")
-
-if [ -z "$(getent passwd|grep ${TARGETUSER})" ]
-then
-   echo "User ${TARGETUSER} not found..."
-   return 1
-fi
-if [ -z "$USERHOME" ]
-then
-   echo "User ${TARGETUSER} not found..."
-   return 1
-fi
-
-echo "    -> Home ${USERHOME}"
-for f in ${FILES}
-do
-   cp -vf "${f}" "${USERHOME}/.${f}"
-   chown "${TARGETUSER}:users" "${USERHOME}/.${f}"
-done
-for d in ${DIRS}
-do
-   if [ ! -d "${USERHOME}/${d}" ]
-   then
-      mkdir "${USERHOME}/${d}"
-      chown "${TARGETUSER}:users" "${USERHOME}/${d}"
-   fi
-done
-return 0
-}
-
-if [ -z "$@" ]
-then
-   echo "Please give at least one username"
-   exit 1
-fi
-
-for i in $@
-do
-   echo "-> User $i"
-   copyfiles ${i}
-done
-echo "Done."
-exit 0
+# Run install
+ansible-playbook -i "local," -c local deploy.yml
