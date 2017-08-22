3e103ef34496d3de05aded6aaf62f45c53c65d73
Lqp1 <lqp1@users.noreply.github.com>
2017-08-22 20:25:48 +0200

    Added Exports, Aliases + improved bash script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+# Check args
+if [ -z $1 ] || [ -z $2 ]
+then
+   echo "USAGE : $0 USER HOME"
+   exit 1
+fi
+
+if [ ! -d $2 ]
+then
+   echo "Path $2 does not lead to a directory..."
+   exit 2
+fi
+
 # Check dependencies
-which ansible-playbook 2>&1 > /dev/null || exit 1
-which git 2>&1 > /dev/null || exit 1
+which ansible-playbook 2>&1 > /dev/null || exit 3
+which git 2>&1 > /dev/null || exit 3
 
 # Run install
-ansible-playbook -i "local," -c local deploy.yml
+mv -v $2/.zshrc $2/.zshrc.old
+ansible-playbook -i "local," -c local --extra-vars "user=$1 home=$2" deploy.yml
